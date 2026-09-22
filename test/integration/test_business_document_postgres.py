4be37f2907de18245f0221f348912ef7d01f37bd
meownm <101579754+meownm@users.noreply.github.com>
2026-09-22 21:31:46 +0300

    test(exports): use confirmed storage cleanup

--- test/integration/test_business_document_postgres.py
+++ test/integration/test_business_document_postgres.py
@@ -911,11 +911,8 @@ def test_real_minio_blob_is_reconciled_after_interruption_immediately_after_put(
         def get(self, bucket, key):
             return minio_storage.get(bucket, key)
 
-        def rm(self, bucket, key):
-            return minio_storage.rm(bucket, key)
-
-        def health(self):
-            return minio_storage.health()
+        def remove_and_confirm_absent(self, bucket, key):
+            return minio_storage.remove_and_confirm_absent(bucket, key)
 
     location = None
     try:
@@ -947,7 +944,7 @@ def test_real_minio_blob_is_reconciled_after_interruption_immediately_after_put(
         assert minio_storage.get(*location) is None
     finally:
         if location is not None:
-            minio_storage.rm(*location)
+            minio_storage.remove_and_confirm_absent(*location)
 
 
 @pytest.mark.p0
